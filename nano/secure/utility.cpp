380515574bd5e4b99ac6f2ea7dc075b2a0fb9409
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2023-03-24 12:45:31 +0100

    Update utility.cpp

--- nano/secure/utility.cpp
+++ nano/secure/utility.cpp
@@ -14,17 +14,17 @@ boost::filesystem::path nano::working_path (nano::networks network)
 		case nano::networks::invalid:
 			release_assert (false);
 			break;
-		case nano::networks::nano_dev_network:
-			result /= "NanoDev";
+		case nano::networks::banano_dev_network:
+			result /= "BananoDev";
 			break;
-		case nano::networks::nano_beta_network:
-			result /= "NanoBeta";
+		case nano::networks::banano_beta_network:
+			result /= "BananoBeta";
 			break;
-		case nano::networks::nano_live_network:
-			result /= "Nano";
+		case nano::networks::banano_live_network:
+			result /= "BananoData";
 			break;
-		case nano::networks::nano_test_network:
-			result /= "NanoTest";
+		case nano::networks::banano_test_network:
+			result /= "BananoTest";
 			break;
 	}
 	return result;
